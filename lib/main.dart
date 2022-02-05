d6bcb7c97ccb20bab72e822554c630c0a44c6813
Gieder Oliveira de Loreto <gieder.gol@gmail.com>
2022-02-05 15:18:20 -0300

    add appbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:newtodolist/myapp.dart';
 import 'package:newtodolist/screens/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'A new todo list',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: HomeScreen(),
-    );
-  }
 }
---
+++ lib/myapp.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:newtodolist/screens/home_screen.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'A new todo list',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
